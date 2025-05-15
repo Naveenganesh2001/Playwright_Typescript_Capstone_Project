6e2a428d2d3198413a1b1c1a74521552651d5bfd
Naveenganesh2001 <ng1332154@gmail.com>
2025-05-15 18:20:01 +0530

    git Actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
      baseURL: 'https://www.makemytrip.com/',
-     headless: false,
+     headless: true,
      ignoreHTTPSErrors: true,
      launchOptions: {
        args: ['--ignore-certificate-errors'],
